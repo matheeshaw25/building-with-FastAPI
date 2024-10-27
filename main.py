a71391730bfb848098a37a88df90e2d64e0e92d0
matheeshaw25 <matheeshawijerathna@gmail.com>
2024-10-27 17:45:36 +0530

    path paramters basics

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/') #path
 def index():
-    return 'hey'
+    return {'data':'blog list'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def about(id:int):
+    #fetch blog with id=id
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments with id=id
+    return {'data':{'1','2'}}
